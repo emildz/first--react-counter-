c97c60ddfc8544001483855b45cbfb1cbfd9f12e
Emil Dzhebrailov <emildz@Emils-MacBook-Pro-2.local>
2022-02-14 11:28:15 -0500

    added coffees from sample api

---
+++ src/Coffees.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+
+function Coffees() {
+    const [coffeeList, setCoffeeList] = useState();
+    useEffect(() => {
+fetch('https://api.sampleapis.com/coffee/hot')
+        .then( response => response.json())
+        .then(data => setCoffeeList(data))
+        .catch(err => console.error(err))
+
+    },[])
+    return (
+        <section className="coffee-container" id= "Coffees">
+        <h2>Coffee Recipes</h2>
+        {!coffeeList
+        ? <h3>Loading...</h3>
+
+        : <ol>{coffeeList.map(coffee => {
+            return <li key={coffee.id}>{coffee.title} </li>
+        } )}
+         </ol>
+}
+        </section>
+    )
+}
+
+export default Coffees;
